1d3c8d4487dd8b5efcc5d2a1c05c0310578bc70c
Boga <purushotham.boga98@gmail.com>
2025-12-21 21:34:41 +0530

    Updated Java Test Script to Version 2.0

--- TestJava.java
+++ TestJava.java
@@ -1,5 +1,6 @@
 public class TestJava {
     public static void main(String[] args) {
-        System.out.println("Java Module - Version 1.0");
+        System.out.println("Java Module - Version 2.0");
+
     }
 }
